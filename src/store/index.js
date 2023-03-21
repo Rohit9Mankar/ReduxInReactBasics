77586285f95b1c993404b997d46150165234acb3
Rohit9Mankar <rohitmankar0079@gmail.com>
2023-03-21 12:23:35 +0530

    increment/decrement by 2 action types

---
+++ src/store/index.js
@@ -0,0 +1,36 @@
+import { createStore } from "redux";
+
+const counterReducer = (state = { counter: 0}, action) => {
+    if(action.type==="decrement"){
+        return {
+            counter: state.counter - 1
+        };
+    }
+
+     if(action.type==="INCREMENTBY2"){
+        return {
+            counter: state.counter + 2
+        };
+    }
+
+    
+
+    if(action.type==="increment"){
+        return {
+            counter: state.counter +1
+        };
+    }
+
+     if(action.type==="DECREMENTBY2"){
+        return {
+            counter: state.counter - 2
+        };
+    }
+
+    return state;
+};
+
+const store=redux.createStore(counterReducer);
+
+export default store;
+
