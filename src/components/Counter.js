a107d216271333eb474beae2d63399fe0d372b99
Rohit9Mankar <rohitmankar0079@gmail.com>
2023-03-21 15:47:27 +0530

    Dispatching actions by useDispatch()

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,40 @@
 import classes from './Counter.module.css';
+import { useSelector , useDispatch} from 'react-redux';
 
 const Counter = () => {
+  const dispatch=useDispatch();
+  const counter= useSelector(state => state.counter);
+
+  const incrementHandler=()=>{
+    dispatch({type : 'increment'});
+  };
+
+  const decrementHandler=()=>{
+    dispatch({type : 'decrement'});
+  };
+
+  const incrementBy5Handler=()=>{
+    dispatch({type : 'INCREMENTBY5'});
+  };
+
+  const decrementBy5Handler=()=>{
+    dispatch({type : 'DECREMENTBY5'});
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <div>
+      <button onClick={incrementBy5Handler}>Increment +5</button>
+        <button onClick={decrementBy5Handler}>Decrement -5</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -7,9 +7,9 @@ const counterReducer = (state = { counter: 0}, action) => {
         };
     }
 
-     if(action.type==="INCREMENTBY2"){
+     if(action.type==="INCREMENTBY5"){
         return {
-            counter: state.counter + 2
+            counter: state.counter + 5
         };
     }
 
@@ -21,16 +21,16 @@ const counterReducer = (state = { counter: 0}, action) => {
         };
     }
 
-     if(action.type==="DECREMENTBY2"){
+     if(action.type==="DECREMENTBY5"){
         return {
-            counter: state.counter - 2
+            counter: state.counter - 5
         };
     }
 
     return state;
 };
 
-const store=redux.createStore(counterReducer);
+const store=createStore(counterReducer);
 
 export default store;
 
